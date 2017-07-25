717c4572a2a8c537d20a2e9ce6eb5ea4c389e985
Christian Palsig <christian.thorpalsig.jensen@skat.dk>
2017-07-25 13:54:17 +0200

    minor edits

--- src/main/java/com/skat/travistesting/Main.java
+++ src/main/java/com/skat/travistesting/Main.java
@@ -3,11 +3,8 @@ package com.skat.travistesting;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         TraMath math = new TraMath();
 
         System.out.println(String.format("Hallo user - {0} and {1}", math.firstNumber(), math.secondNumber()));
     }
-
-
 }
